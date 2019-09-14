828bc3359895f2b8b53dbff4ec319e1c7f23bd51
kjolencz <43276807+kjolencz@users.noreply.github.com>
2019-09-14 14:02:32 -0400

    hehe

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
 var request = new XMLHttpRequest();
 request.open(
   "GET",
-  "https://api.edamam.com/api/food-database/parser?nutrition-type=logging&ingr=red%20apple&app_id={c3ed774e}&app_key={f8f0169410dd2898d23c7a565d69469c",
+  "https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id={c3ed774e}&app_key={06cbdea958770dde2eeb5d1ae551a11e	}",
   true
 );
 request.onload = function() {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = movie.title;
-
-      const p = document.createElement("p");
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
-
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-    });
-  } else {
-    const errorMessage = document.createElement("marquee");
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
+  console.log(data);
 };
-
 request.send();
